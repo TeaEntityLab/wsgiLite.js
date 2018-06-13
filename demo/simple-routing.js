369c0ff14f7fbeaf36c189488ce096576dffbfc0
John Lee <johnteee@gmail.com>
2018-06-13 18:12:07 +0800

    Define static files serving.

--- demo/simple-routing.js
+++ demo/simple-routing.js
@@ -1,4 +1,7 @@
-const {WSGILite} = require('../wsgilite');
+const {
+  WSGILite,
+  defServeFileStatic,
+} = require('../wsgilite');
 
 const server = new WSGILite();
 
@@ -10,6 +13,9 @@ server.addRoute('/', (request, response, meta)=>{
 });
 server.addRoute('/user/:id', (request, response, meta)=>{
   return meta; // {"path":"/user/theID","msg":"I got it","id":"theID"}
+});
+server.addRoute('/file/*relativePath', (request, response, meta)=>{
+  defServeFileStatic('demo')(request, response, meta);
 });
 server.addRoute('/heartbeat', (request, response, meta)=>{
   return "ok"; // ok
--- wsgilite.js
+++ wsgilite.js
@@ -1,11 +1,81 @@
-var url = require("url");
+var url = require('url');
+var path = require('path');
+
+var fs = require('fs');
+
 var http = require('http');
 var RouteParser = require('route-parser');
 
+// maps file extention to MIME typere
+const mimeMap = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword'
+};
+
+function extendMeta(meta, addition) {
+  return Object.assign(meta, addition);
+}
 function addRequestInfosToMeta(request, response, meta) {
-  Object.assign(meta, {
+  extendMeta(meta, {
     path: url.parse(request.url).pathname,
   });
+}
+function defineNoCORS(methods) {
+  methods = methods ? methods : ['GET','POST','OPTIONS','PUT','PATCH','DELETE'];
+  return function (request, response, meta) {
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', methods.join(', '));
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
+  }
+}
+function defServeFileStatic(baseDir) {
+  baseDir = baseDir ? baseDir : '.';
+
+  return function (request, response, meta) {
+    const pathname = meta.relativePath ? meta.relativePath : meta.path;
+    const ext = path.parse(pathname).ext;
+    const finalPath = `${__dirname}/${baseDir}/${pathname}`;
+
+    console.log({finalPath});
+
+    fs.exists(finalPath, function (exist) {
+      if(!exist) {
+        return;
+      }
+
+    // read file from file system
+    fs.readFile(finalPath, function(err, data){
+      if(err){
+        response.statusCode = 500;
+        response.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        // response.setHeader('Content-type', mimeMap[ext] || 'text/plain' );
+        console.log(data);
+        response.end(data);
+      }
+    });
+  });
+  }
 }
 
 class Route {
@@ -19,7 +89,7 @@ class Route {
   matches(request, response, meta) {
     var matchesAndParam = this.routeParser.match(url.parse(request.url).pathname);
     if (matchesAndParam) {
-      var result = this.fn(request, response, Object.assign(meta, matchesAndParam));
+      var result = this.fn(request, response, extendMeta(meta, matchesAndParam));
       if (result) {
         response.end(typeof result === 'string' ? result : JSON.stringify(result));
       }
@@ -97,4 +167,9 @@ class WSGILite {
 module.exports = {
   Route,
   WSGILite,
+
+  addRequestInfosToMeta,
+  defineNoCORS,
+  defServeFileStatic,
+  extendMeta,
 };
