824bf4dc854a01909754bfb7c0e7c2975afae905
John Lee <johnteee@gmail.com>
2018-06-25 19:15:36 +0800

    Implement promise style file

--- file.js
+++ file.js
@@ -5,6 +5,8 @@ const sep = path.sep;
 const {
   actionMetaSkip404,
   actionMetaDoFnAndKeepConfigs,
+
+  MiddlewareDefault404,
 } = require('./common');
 
 // maps file extention to MIME typere
@@ -25,39 +27,55 @@ const mimeMap = {
 
 module.exports = {
   mimeMap,
-  defMiddlewareServeFileStatic: function (baseDir) {
+  defMiddlewareServeFileStatic: function (baseDir, middleware404) {
     baseDir = baseDir ? baseDir : '.';
+    middleware404 = middleware404 ? middleware404 : (request, response, meta)=>MiddlewareDefault404(request, response, meta)(false);
 
     return function (request, response, meta) {
       const pathname = meta.relativePath ? meta.relativePath : meta._url_path;
       const ext = path.parse(pathname).ext;
       const finalPath = `${process.cwd()}${sep}${baseDir}${sep}${pathname}`;
 
+      /*
       var exist = fs.existsSync(finalPath);
       if(!exist) {
         actionMetaSkip404(meta, true);
         return;
       }
+      */
       actionMetaSkip404(meta);
 
-      response.writeHead(200, {
-        'Transfer-Encoding': 'chunked',
-        'Content-type': mimeMap[ext] || 'text/plain',
-        'X-Content-Type-Options': 'nosniff',
-      });
-      // read file from file
+      return new Promise(function(resolve, reject) {
+        // read file from file
+        var stream = fs.createReadStream(finalPath);
 
-      var stream = fs.createReadStream(finalPath);
+        stream.on('open', () => {
+          response.writeHead(200, {
+            'Transfer-Encoding': 'chunked',
+            'Content-type': mimeMap[ext] || 'text/plain',
+            'X-Content-Type-Options': 'nosniff',
+          });
+        });
+        stream.on('data', (chunk) => {
+          response.write(chunk);
+        });
+        stream.on('error', function(err){
+          reject(err);
+        });
+        stream.on('end', () => {
+          response.end();
 
-      stream.on('data', (chunk) => {
-        response.write(chunk);
-      });
-      stream.on('error', function(err){
-        response.statusCode = 500;
-        response.end(`Error getting the file: ${err}.`);
-      });
-      stream.on('end', () => {
-        response.end();
+          resolve(true);
+        });
+      }).catch((e)=>{
+        if (e && e.code && e.code === 'ENOENT') {
+          middleware404(request, response, meta);
+        } else {
+          console.log(e);
+          response.statusCode = 500;
+          response.end(`Error getting the file: ${pathname}.`);
+        }
+        return e;
       });
     }
   },
