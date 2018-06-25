0b1b8e71966ab13c971d14aac52fe315aa5086d7
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-06-25 21:34:27 +0800

    Extract defMiddlewareServeFileStatic as reusable function. Fix empty relativePath checking

--- file.js
+++ file.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const sep = path.sep;
 
+const Maybe = require('fpEs/maybe');
+
 const {
   actionMetaSkip404,
   actionMetaDoFnAndKeepConfigs,
@@ -25,63 +27,65 @@ const mimeMap = {
   '.doc': 'application/msword'
 };
 
+function defMiddlewareServeFileStatic(baseDir, doRawExceptionReturn) {
+  baseDir = baseDir ? baseDir : '.';
+
+  return function (request, response, meta) {
+    const pathname = Maybe.just(meta.relativePath).isPresent() ? meta.relativePath : meta._url_path;
+    const ext = path.parse(pathname).ext;
+    const finalPath = `${process.cwd()}${sep}${baseDir}${sep}${pathname}`;
+
+    /*
+    var exist = fs.existsSync(finalPath);
+    if(!exist) {
+      actionMetaSkip404(meta, true);
+      return;
+    }
+    */
+    actionMetaSkip404(meta);
+
+    let promise = new Promise(function(resolve, reject) {
+      // read file from file
+      var stream = fs.createReadStream(finalPath);
+
+      stream.on('open', () => {
+        response.writeHead(200, {
+          'Transfer-Encoding': 'chunked',
+          'Content-type': mimeMap[ext] || 'text/plain',
+          'X-Content-Type-Options': 'nosniff',
+        });
+      });
+      stream.on('data', (chunk) => {
+        response.write(chunk);
+      });
+      stream.on('error', function(err){
+        reject(err);
+      });
+      stream.on('end', () => {
+        response.end();
+
+        resolve(true);
+      });
+    });
+
+    if (doRawExceptionReturn) {
+      return promise;
+    }
+
+    return promise.catch((e)=>{
+      if (e && e.code && e.code === 'ENOENT') {
+        MiddlewareDefault404(request, response, meta)(false);
+      } else {
+        console.log(e);
+        response.statusCode = 500;
+        response.end(`Error getting the file: ${pathname}.`);
+      }
+      return e;
+    });
+  }
+}
+
 module.exports = {
   mimeMap,
-  defMiddlewareServeFileStatic: function (baseDir, doRawExceptionReturn) {
-    baseDir = baseDir ? baseDir : '.';
-
-    return function (request, response, meta) {
-      const pathname = meta.relativePath ? meta.relativePath : meta._url_path;
-      const ext = path.parse(pathname).ext;
-      const finalPath = `${process.cwd()}${sep}${baseDir}${sep}${pathname}`;
-
-      /*
-      var exist = fs.existsSync(finalPath);
-      if(!exist) {
-        actionMetaSkip404(meta, true);
-        return;
-      }
-      */
-      actionMetaSkip404(meta);
-
-      let promise = new Promise(function(resolve, reject) {
-        // read file from file
-        var stream = fs.createReadStream(finalPath);
-
-        stream.on('open', () => {
-          response.writeHead(200, {
-            'Transfer-Encoding': 'chunked',
-            'Content-type': mimeMap[ext] || 'text/plain',
-            'X-Content-Type-Options': 'nosniff',
-          });
-        });
-        stream.on('data', (chunk) => {
-          response.write(chunk);
-        });
-        stream.on('error', function(err){
-          reject(err);
-        });
-        stream.on('end', () => {
-          response.end();
-
-          resolve(true);
-        });
-      });
-
-      if (doRawExceptionReturn) {
-        return promise;
-      }
-
-      return promise.catch((e)=>{
-        if (e && e.code && e.code === 'ENOENT') {
-          MiddlewareDefault404(request, response, meta)(false);
-        } else {
-          console.log(e);
-          response.statusCode = 500;
-          response.end(`Error getting the file: ${pathname}.`);
-        }
-        return e;
-      });
-    }
-  },
+  defMiddlewareServeFileStatic,
 };
