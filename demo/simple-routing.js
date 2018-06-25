0f5b879dd4edff33018c02b0872cf7d6d12a3d65
John Lee <johnteee@gmail.com>
2018-06-25 11:40:11 +0800

    Add comments & README.md

--- demo/simple-routing.js
+++ demo/simple-routing.js
@@ -15,18 +15,22 @@ const {
   generateCSRFFormInput,
 } = require('../csrf');
 
+// Settings
+
 const server = new WSGILite({
-  secret: 'abcdefg',
-  logProcessMessage: true,
-  debug: true,
+  secret: 'abcdefg', // The secret key for CSRF
+  logProcessMessage: true, // Log the multiple processes starting/ending messages.
+  debug: true, // Debug mode, it will show stacktrace of errors in the responses
 
   workerServeTimesToRestart: 500, // Each child worker will auto-restart after it served 500 requests
 });
-const redirect = server.redirectAsFunction();
+const redirect = server.redirectAsFunction(); // Define redirect() in simple & shorter function style.
 const template = new Template({
-  baseDir: "demo/template",
+  baseDir: "demo/template", // Define the base dir of template files
 });
-const render = template.renderAsFunction();
+const render = template.renderAsFunction(); // Define render() in simple & shorter function style.
+
+// Middlewares
 
 server.addMiddleware(async (request, response, meta)=>{
   meta.msg3 = 'I got it3';
@@ -37,43 +41,62 @@ server.addMiddleware(function * (request, response, meta) {
 server.addMiddleware((request, response, meta)=>{
   meta.msg = 'I got it';
 });
+// CSRF routes defining
 server.addMiddleware(defFormCsrfCheckRoutes([
-  '/upload',
+  '/upload', // Your can define more paths here
 ], server));
 server.addMiddleware(defHeaderCsrfCheckRoutes([
-  '/upload2',
+  '/upload2', // Your can define more paths here
 ], server));
+
+// Routes
+
+// Print the `meta` object
 server.GET('/', (request, response, meta)=>{
   return JSON.stringify(meta); // {"_skip404":true,"_url_path":"/","msg3":"I got it3","msg2":"I got it2","msg":"I got it"}
 });
+
+// The server will terminate
 server.GET('/terminate', (request, response, meta)=>{
   response.end("terminate");
   server.terminate();
 });
+
+// Path parameters(until `/` or `?` or the end)
 server.GET('/user/:id', function *(request, response, meta) {
   return yield Promise.resolve(meta); // {"_skip404":true,"_url_path":"/user/theID","msg3":"I got it3","msg2":"I got it2","msg":"I got it","id":"theID"}
 });
+// Path parameters(until `?` or the end)
 server.GET('/file/*relativePath', (request, response, meta)=>{
   defMiddlewareServeFileStatic('demo')(request, response, meta);
 });
+
+// Show the CSRF usages (token only & a form hidden input with the token)
 server.GET('/csrf', function *(request, response, meta) {
   return getCSRF_token(request, response) + ` ${generateCSRFFormInput(request, response)}`; // CSRF_token
 });
+// CSRF checking (form)
 server.POST('/upload', (request, response, meta)=>{
   return 'CSRF_token ok'; // CSRF_token ok
 });
+// CSRF checking (header)
 server.POST('/upload2', (request, response, meta)=>{
   return 'x-csrf-token ok'; // x-csrf-token ok
 });
 server.GET('/heartbeat', async (request, response, meta)=>{
   return "ok"; // ok
 });
+
+// Redirect to an existing route
 server.GET('/heartbeat2', async (request, response, meta)=>{
   redirect('/heartbeat')(request, response, meta); // ok
 });
+// Redirect to a non-existing route(will 404)
 server.GET('/heartbeat3', async (request, response, meta)=>{
   redirect('/heartbeat999')(request, response, meta); // 404 File not found.
 });
+
+// Nested routing definitions
 server.defSubRoute('test', function (defSub) {
   defSub.defSubRoute('change', function (defSub) {
     defSub.defSubRoute('for', function (defSub) {
@@ -83,6 +106,8 @@ server.defSubRoute('test', function (defSub) {
     });
   });
 });
+
+// Template usages
 server.GET('/template', async (request, response, meta)=>{
   return render("features", {
             "title": "JavaScript Templates",
@@ -103,6 +128,7 @@ server.GET('/template', async (request, response, meta)=>{
 server.GET('/exception', async function (request, response, meta) {
   throw new Error("There's an exception"); // Error: There's an exception
 });
+
 // Timeout
 let routeTimeout = server.GET('/timeout', async function (request, response, meta) {
   var rp = require('request-promise-native');
