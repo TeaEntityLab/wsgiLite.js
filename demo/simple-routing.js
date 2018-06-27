bf62e18eb30642ee0e1287eadde6c91de0faf32f
John Lee <johnteee@gmail.com>
2018-06-27 13:59:01 +0800

    Rename example variable names.

--- demo/simple-routing.js
+++ demo/simple-routing.js
@@ -18,7 +18,7 @@ const {
 
 // Settings
 
-const server = new WSGILite({
+const wsgilite = new WSGILite({
   secret: 'abcdefg', // The secret key for CSRF
   logProcessMessage: true, // Log the multiple processes starting/ending messages.
   debug: true, // Debug mode, it will show stacktrace of errors in the responses
@@ -27,14 +27,14 @@ const server = new WSGILite({
   processNum: require('os').cpus().length, // N + 1 processes (cluster: 1 * master + n * fastcgi style http/https serving)
   // processNum: 0, // Single process
   // createServerOptions: {}, // Additional createServer options for http/https.createServer(options)
-  onServerCreated: function (server) {
-    server.timeout = 60*1000;
+  onServerCreated: function (httpServer) {
+    httpServer.timeout = 60*1000;
     console.log('server.timeout set');
   }, // Callback on one of servers created(this will be called in Multiple processes)
 
   workerServeTimesToRestart: 500, // Each child worker will auto-restart after it served 500 requests
 });
-const redirect = server.redirectAsFunction(); // Define redirect() in simple & shorter function style.
+const redirect = wsgilite.redirectAsFunction(); // Define redirect() in simple & shorter function style.
 const template = new Template({
   baseDir: "demo/template", // Define the base dir of template files
 });
@@ -42,72 +42,72 @@ const render = template.renderAsFunction(); // Define render() in simple & short
 
 // Middlewares
 
-server.addMiddleware(async (request, response, meta)=>{
+wsgilite.addMiddleware(async (request, response, meta)=>{
   meta.msg3 = 'I got it3';
 });
-server.addMiddleware(function * (request, response, meta) {
+wsgilite.addMiddleware(function * (request, response, meta) {
   return Promise.resolve(0).then(()=>meta.msg2 = 'I got it2');
 });
-server.addMiddleware((request, response, meta)=>{
+wsgilite.addMiddleware((request, response, meta)=>{
   meta.msg = 'I got it';
 });
 // CSRF routes defining
-server.addMiddleware(defFormCsrfCheckRoutes([
+wsgilite.addMiddleware(defFormCsrfCheckRoutes([
   '/upload', // Your can define more paths here
-], server));
-server.addMiddleware(defHeaderCsrfCheckRoutes([
+], wsgilite));
+wsgilite.addMiddleware(defHeaderCsrfCheckRoutes([
   '/upload2', // Your can define more paths here
-], server));
+], wsgilite));
 
 // Routes
 
 // Print the `meta` object
-server.GET('/', (request, response, meta)=>{
+wsgilite.GET('/', (request, response, meta)=>{
   return JSON.stringify(meta); // {"_skip404":true,"_url_path":"/","msg3":"I got it3","msg2":"I got it2","msg":"I got it"}
 });
 
 // The server will terminate
-server.GET('/terminate', (request, response, meta)=>{
+wsgilite.GET('/terminate', (request, response, meta)=>{
   response.end("terminate");
-  server.terminate();
+  wsgilite.terminate();
 });
 
 // Path parameters(until `/` or `?` or the end)
-server.GET('/user/:id', function *(request, response, meta) {
+wsgilite.GET('/user/:id', function *(request, response, meta) {
   return yield Promise.resolve(meta); // {"_skip404":true,"_url_path":"/user/theID","msg3":"I got it3","msg2":"I got it2","msg":"I got it","id":"theID"}
 });
 // Path parameters(until `?` or the end)
-server.GET('/file*relativePath', (request, response, meta)=>{
+wsgilite.GET('/file*relativePath', (request, response, meta)=>{
   defMiddlewareServeFileStatic('demo')(request, response, meta);
 });
 
 // Show the CSRF usages (token only & a form hidden input with the token)
-server.GET('/csrf', function *(request, response, meta) {
+wsgilite.GET('/csrf', function *(request, response, meta) {
   return getCSRF_token(request, response) + ` ${generateCSRFFormInput(request, response)}`; // CSRF_token
 });
 // CSRF checking (form)
-server.POST('/upload', (request, response, meta)=>{
+wsgilite.POST('/upload', (request, response, meta)=>{
   return 'CSRF_token ok'; // CSRF_token ok
 });
 // CSRF checking (header)
-server.POST('/upload2', (request, response, meta)=>{
+wsgilite.POST('/upload2', (request, response, meta)=>{
   return 'x-csrf-token ok'; // x-csrf-token ok
 });
-server.GET('/heartbeat', async (request, response, meta)=>{
+wsgilite.GET('/heartbeat', async (request, response, meta)=>{
   return "ok"; // ok
 });
 
 // Redirect to an existing route
-server.GET('/heartbeat2', async (request, response, meta)=>{
+wsgilite.GET('/heartbeat2', async (request, response, meta)=>{
   redirect('/heartbeat')(request, response, meta); // ok
 });
 // Redirect to a non-existing route(will 404)
-server.GET('/heartbeat3', async (request, response, meta)=>{
+wsgilite.GET('/heartbeat3', async (request, response, meta)=>{
   redirect('/heartbeat999')(request, response, meta); // 404 File not found.
 });
 
 // Nested routing definitions
-server.defSubRoute('test', function (defSub) {
+wsgilite.defSubRoute('test', function (defSub) {
   defSub.defSubRoute('change', function (defSub) {
     defSub.defSubRoute('for', function (defSub) {
       defSub.GET('it', async (request, response, meta)=>{
@@ -118,7 +118,7 @@ server.defSubRoute('test', function (defSub) {
 });
 
 // Template usages
-server.GET('/template', async (request, response, meta)=>{
+wsgilite.GET('/template', async (request, response, meta)=>{
   return render("features", {
             "title": "JavaScript Templates",
             "url": "https://github.com/blueimp/JavaScript-Templates",
@@ -135,18 +135,18 @@ server.GET('/template', async (request, response, meta)=>{
 });
 
 // Exception
-server.GET('/exception', async function (request, response, meta) {
+wsgilite.GET('/exception', async function (request, response, meta) {
   throw new Error("There's an exception"); // Error: There's an exception
 });
 
 // Timeout
-let routeTimeout = server.GET('/timeout', async function (request, response, meta) {
+let routeTimeout = wsgilite.GET('/timeout', async function (request, response, meta) {
   var rp = require('request-promise-native');
   await rp.get('https://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_30mb.mp4');
   return "ok";
 });
 routeTimeout.timeout = 5000;
 
-server.listen(3333, 'localhost', function () {
+wsgilite.listen(3333, 'localhost', function () {
   console.log('Server up');
 });
